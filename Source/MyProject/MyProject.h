46961d7c16e33eedcb55e64506019bb4136726eb
bratan <maalaoui.ahmed.am75@gmail.com>
2025-07-27 14:44:14 +0100

    Add debug line macro and update item debug visuals
    Introduced DRAW_LINE macro for drawing debug lines in red and updated DRAW_SPHERE to use red color. Simplified AItem debug output by removing on-screen and log messages, now only drawing a sphere and a line at the item's location and forward direction.

--- Source/MyProject/MyProject.h
+++ Source/MyProject/MyProject.h
@@ -4,4 +4,5 @@
 
 #include "CoreMinimal.h"
 
-#define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Green, true);
+#define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, true);
+#define DRAW_LINE(StartLocation, EndLocation) if(GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1, 0, 1.f );
--- Source/MyProject/Private/Items/Item.cpp
+++ Source/MyProject/Private/Items/Item.cpp
@@ -5,7 +5,7 @@
 #include "DrawDebugHelpers.h"
 #include "MyProject/MyProject.h"
 
-#define THIRTY 30
+
 
 // Sets default values
 AItem::AItem()
@@ -19,15 +19,14 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Warning, TEXT("Begin Play called"));
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1,60.f, FColor::Cyan, FString("Item on Screen message"));
-	}
-	UWorld* World= GetWorld();
+	
 	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+	UWorld* World= GetWorld();
+	
+	
 	DRAW_SPHERE(Location);
+	DRAW_LINE(Location,Location+Forward*100.f);
 	
 	
 }
@@ -36,14 +35,7 @@ void AItem::BeginPlay()
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime);
-	if (GEngine)
-	{
-		FString Name= GetName();
-		FString Message = FString::Printf(TEXT("Item Name : %s"), *Name);
-		GEngine->AddOnScreenDebugMessage(1,60.f, FColor::Cyan, Message);
-		UE_LOG(LogTemp, Warning, TEXT("Item Name %s"), *Name);
-	}
+	
 
 }
 
